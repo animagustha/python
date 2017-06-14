44a078e5a8d5acfac6375ff4d4a49c26461e14cf
haseeb <akthar.haseeb@gmail.com>
2017-06-14 11:50:55 -0700

    my first python program on git

---
+++ ifElseStatements.py
@@ -0,0 +1,18 @@
+age = 20
+if age>18:
+    print('you are eligible for license')
+
+a = [40, 31, 8]
+if a[0] > a[1]:
+    if a[0] < a[2]:
+        print(a[0], " is the greatest num")
+
+    else:
+        print(a[2], " is the greatest num")
+
+elif a[1]>a[2]:
+    print(a[1], "is the greatest num")
+
+else:
+    print(a[2], "is the greatest num")
+
