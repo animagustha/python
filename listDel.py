d1180d762f071f2716b5bc48b1e2dc9a00bae9b7
animagustha <akhtar.haseeb@gmail.com>
2017-09-27 16:09:04 -0700

    delete an element from the list

---
+++ listDel.py
@@ -0,0 +1,6 @@
+teen_wolf = ['scott', 101, 'allison', 103, 'stylles', 107]
+print(teen_wolf)
+del(teen_wolf[2:4])
+print(teen_wolf)
+del(teen_wolf[3])
+print(teen_wolf)
---
+++ listModify.py
@@ -0,0 +1,5 @@
+teen_wolf = ['scott', 101, 'allison', 103, 'stylles', 107]
+print(teen_wolf)
+# modify the value of allison's id
+teen_wolf[3] = 102
+print(teen_wolf)
