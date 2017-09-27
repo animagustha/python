ccfb93dc82b403ca19cdc90ecf3bdc5b893cc8fb
animagustha <akhtar.haseeb@gmail.com>
2017-09-27 13:04:26 -0700

    program that raises base 'a' to power 'b'

---
+++ exponents.py
@@ -0,0 +1,4 @@
+# a raised to the powers of b
+a = int(input("enter the base "))  # input is in string format therefore it's type has to be converted to int
+b = int(input("enter the power to which the base has to be raised to "))
+print("result" + str(a ** b))
